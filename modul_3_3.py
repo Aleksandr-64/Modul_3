e6eeff2a2d3ddba595bc0f134be14c868d6748c7
Aleksandr Kol <sany199910@mail.ru>
2024-09-26 14:49:12 +0400

    Add files via upload

---
+++ modul_3_3.py
@@ -0,0 +1,20 @@
+#1
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+print_params('Vasya','Petya','Dimon')
+print_params(4,'Books')
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
+#2
+values_list = 3, 'soft', False
+values_dict = {'a': 2, 'b': 'Хорошо', 'c': (3,5,6)}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+#3
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
