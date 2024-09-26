6f9170ab0da8b48dea0f380861e6698f20efb955
Aleksandr Kol <sany199910@mail.ru>
2024-09-26 15:10:38 +0400

    2

--- modul_3_3.py
+++ modul_3_3.py
@@ -1,5 +1,5 @@
-#1
-def print_params(a = 1, b = 'строка', c = True):
+print(),print('Задание 1:')
+def print_params(a = 1, b = 'строка', c = True): #1
     print(a, b, c)
 
 print_params()
@@ -8,12 +8,16 @@ print_params(4,'Books')
 print_params(b = 25)
 print_params(c = [1, 2, 3])
 
+print(),print('Задание 2:')
+
 #2
 values_list = 3, 'soft', False
 values_dict = {'a': 2, 'b': 'Хорошо', 'c': (3,5,6)}
 
 print_params(*values_list)
 print_params(**values_dict)
+
+print(),print('Задание 3:')
 
 #3
 values_list_2 = [54.32, 'Строка' ]
